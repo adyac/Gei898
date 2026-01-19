3dfec4a8ae7c39673782b1a2abd771c64a6b5459
ac <andrei_mtl@yahoo.ca>
2026-01-19 14:05:02 -0500

    Almost done code, works in kazems branch

---
+++ Devoir 1/Network.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_dim=9):
+        # Super fait en sorte que la classe Autoencoder peut
+        # utiliser les méthodes de la classe fondatrice Module
+        super(Autoencoder, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 32),
+            nn.ReLU(),
+            nn.Linear(32, 7),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(7, 32),
+            nn.ReLU(),
+            nn.Linear(32, input_dim)
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        y_hat = self.decoder(z)
+        return y_hat
